447cf31306c11e0ee0cd56ea47a15935a020ccbf
Eric <e.dambrin@gmail.com>
2021-06-01 09:36:12 +0200

    ok

--- js/articles.js
+++ js/articles.js
@@ -3,13 +3,6 @@ class Article{
          cameras && Object.assign(this,cameras);
        
     }
-    
-     showCamera (data) {
-     // Création des éléments
-     let name = document.querySelector("#name"),
-       price = document.querySelector("#price"),
-       description = document.querySelector("#description"),
-       image = document.querySelector("#image"),
-       selectLenses = document.querySelector("select");
-     }
+   
+
 }
