6d8d27f7f43a5e0b8da3a3458d50b7091faa472a
Eric <e.dambrin@gmail.com>
2021-06-30 15:49:13 +0200

    Annalyses et amélioration de la pertinence des annotations

--- js/panier.js
+++ js/panier.js
@@ -1,7 +1,7 @@
 //Récupération des informations du produit selectionné envoyé dans le local storage
 let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
 
-                //.............Affichage produits panier..............//
+//.............Affichage produits panier..............//
 
 const positionElement = document.querySelector("#produitChoisi");
 
@@ -25,23 +25,24 @@ if (produitLocalStorage === null) {
 
 
     }//Pour mettre l'ensemble des produits du panier visible sur la page
-    if (i == produitLocalStorage.length) { 
+    if (i == produitLocalStorage.length) {
         positionElement.innerHTML = structureProduitPanier;
     }
 
 }
 
 
-
+//Déclaration de la variable "form" pour la sélection du formulaire
 let form = document.querySelector(".champ_a_remplir");
 
+// Écoute de la valeur écrite dans le champs de l'Email par l'utilisateur
 form.email.addEventListener('change', (e) => {
 
-    validEmail(e.target.value);
-
+    validEmail(e.target.value);// l'élément 'target' déclenche l'évènement à sa juste valeur 'value'
+    // Demander plus d'explications...??
 
 });
-
+// Validation de l'Email selon une expression régulière
 const validEmail = function (inputEmail) {
     let emailRegExp =
         /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/
@@ -49,7 +50,7 @@ const validEmail = function (inputEmail) {
     let testEmail = emailRegExp.test(inputEmail);
 
     if (testEmail == true) {
-        document.querySelector('small').innerHTML = 'Adresse valide'
+        document.querySelector('small').innerHTML = 'Ok'
         document.querySelector('small').style.color = 'black'
     } else {
         document.querySelector('small').innerHTML = 'Adresse non valide'
@@ -59,20 +60,20 @@ const validEmail = function (inputEmail) {
 }
 
 
-Array.from(form.querySelectorAll('input[type="text"')).forEach(elt =>{// Je pense que tout ce code ne sert à rien??
+Array.from(form.querySelectorAll('input[type="text"')).forEach(elt => {// Je pense que tout ce code ne sert à rien??
     elt.addEventListener('focusout', (e) => {// Je pense que tout ce code ne sert à rien??
         console.log(e.target.value)// Je pense que tout ce code ne sert à rien??
         validFormat(e.target);//Je ne comprend pas??// Je pense que tout ce code ne sert à rien??
-    
+
     }) // Je pense que tout ce code ne sert à rien??
 })
 
-const validFormat = function (input) { 
+const validFormat = function (input) {
 
     if (input.value.trim() === '') {
-        //trim permet de prendre en compte lorsque l'on fait une barre espace n'est pas une valeur inscrite..
-        console.log(input.parentNode);
-        //parentNode permet de dire si le format est bon ou pas sous le champs concerné
+        //trim permet de prendre en compte lorsque l'on fait une barre espace que ce n'est pas une valeur inscrite dans le champs au même titre qu'une lettre..
+
+        //parentNode permet de dire si le format est bon ou pas SOUS le champs concerné
         input.parentNode.querySelector(".format").innerHTML = "Format non-valide"
         return false
     } else {
@@ -81,37 +82,36 @@ const validFormat = function (input) {
     return true;
 }
 //Récupération des prix des produits contenus dans le panier :
-let prixDeLaCommande= [];
-for(i=0 ; i<produitLocalStorage.length ; i++){
-    
-prixDeLaCommande.push(produitLocalStorage[i].prix)
+let prixDeLaCommande = [];
+for (i = 0; i < produitLocalStorage.length; i++) {
 
-    // console.log(prixDeLaCommande);
+    prixDeLaCommande.push(produitLocalStorage[i].prix)
+
+    console.log(prixDeLaCommande);
 }
 // Addition pour obtenir prix total du panier avec la méthode reduce :
-
-const reducer = (accumulator,currentValue)=> accumulator + currentValue
+const reducer = (accumulator, currentValue) => accumulator + currentValue
 // console.log(prixDeLaCommande.reduce(reducer));
 
 
 bouton.addEventListener("click", (e) => {
     e.preventDefault();
     let form = document.querySelector(".champ_a_remplir");
-    if(!validEmail(form.email.value)){
+    if (!validEmail(form.email.value)) {
         alert("erreur email");
         return false
     }
     let isOk = true;
-    Array.from(form.querySelectorAll('input[type="text"')).forEach(elt =>{
-        
-            if(!validFormat(elt)){
-        // demander si sans les return à la function de validFormat celà fonctionne??
-                alert("Le champ " + elt.placeholder + " est vide")
-                isOk = false;// Je comprend pas les isOk??
-            }
-        
+    Array.from(form.querySelectorAll('input[type="text"')).forEach(elt => {
+
+        if (!validFormat(elt)) {
+            // demander si sans les return à la function de validFormat celà fonctionne??
+            alert("Le champ " + elt.placeholder + " est vide")
+            isOk = false;// Je comprend pas les isOk??
+        }
+
     })
-    if(!isOk){
+    if (!isOk) {
         return false
     }
 
@@ -130,12 +130,10 @@ bouton.addEventListener("click", (e) => {
         e.preventDefault();
         document.getElementById("erreur").innerHTML = erreur
     } else {
-     
-        alert('Formulaire enregistré !');
 
-    }
-   
-    
+        }
+
+
     //--------------Récupération des donnés du formulaire ----------------------
 
     const contact = {
@@ -149,58 +147,58 @@ bouton.addEventListener("click", (e) => {
     //----------------Mettre les donnés du formulaire dans le localStorage-------------
     localStorage.setItem("newUser", JSON.stringify(contact));
     let products = [];
-    produitLocalStorage.forEach(conne=> products.push(conne.id))
+    produitLocalStorage.forEach(produit => products.push(produit.id))
     // -----création de la variable contenant les produits du panier et les infos du formulaires--------
     let infosServeur = {
         contact,
         products
     }
-    
+
     // --------Envoi des informations du panier avec la méthode Post vers le serveur-------
-       
-     const promiseCommande = fetch("http://localhost:3000/api/cameras/order", {
+
+    const promiseCommande = fetch("http://localhost:3000/api/cameras/order", {
         method: "POST",
         body: JSON.stringify(infosServeur),
         headers: { "Content-type": "application/json; charset=UTF-8" },
-        
+
     })
     //Pour voir la commande dans la console :
 
-    promiseCommande.then(async(response)=>{
+    promiseCommande.then(async (response) => {
         try {
-            
+
             const contenu = await response.json();
             console.log(contenu)
-            localStorage.setItem("orderId",contenu.orderId) //id de la commande dans le local storage
-            localStorage.setItem('prixDeLaCommande',prixDeLaCommande.reduce(reducer));
-        } 
+            localStorage.setItem("orderId", contenu.orderId) //id de la commande dans le local storage
+            localStorage.setItem('prixDeLaCommande', prixDeLaCommande.reduce(reducer));
+        }
         catch (error) {
-            
+
             console.log(error)
         }
     })
     // prix total de la commande :
 
-  
+
 
     // Envoyer commande dans la page confirmation commande :
 
-  window.location = "confirmation commande.html";
-  
+    window.location = "confirmation commande.html";
 
 
-//     let btnEnregistrer = document.querySelector("#btn");
-//     btnSelection.addEventListener("click", (event) => {
-//         event.preventDefault()
-// let commandeLocalStorage = localStorage.getItem("commande") ? JSON.parse(localStorage.getItem("commande")) : [];
-    
-    
-    
-        // .then(response => response.json())
-        // .then(json => console.log(json))
-        // .catch(err => console.log(err));
-    
-       
+
+    //     let btnEnregistrer = document.querySelector("#btn");
+    //     btnSelection.addEventListener("click", (event) => {
+    //         event.preventDefault()
+    // let commandeLocalStorage = localStorage.getItem("commande") ? JSON.parse(localStorage.getItem("commande")) : [];
+
+
+
+    // .then(response => response.json())
+    // .then(json => console.log(json))
+    // .catch(err => console.log(err));
+
+
 })
 
 
